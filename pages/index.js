b7e24821c9510f7137190427ce0d44eba455d121
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-04-22 00:11:03 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>HULU CLONE WITH NEXTJS</title>
+        <title>HULU CLONE WITH NEXT JS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
